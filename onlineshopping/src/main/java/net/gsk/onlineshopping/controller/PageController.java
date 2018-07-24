a46c021c67c7315528123587c9d4daaf32ff38eb
santoshkumar.guntur <santoshkumar.guntur@N9659.intellectdesign.com>
2018-07-24 17:47:34 +0530

    Part 02 - 04 - Understanding @RequestParam and @PathVariable annotations

--- onlineshopping/src/main/java/net/gsk/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/gsk/onlineshopping/controller/PageController.java
@@ -13,4 +13,23 @@ public class PageController {
 		mv.addObject("greeting", "Welcome to spring MVC");
 		return mv;
 	}
+	
+ /*   @RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting){
+    	if(greeting ==null)
+    		greeting ="Welcome to Spring MVC";
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+    
+    @RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting") String greeting){
+    	if(greeting ==null)
+    		greeting ="Welcome to Spring MVC";
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+*/
 }
