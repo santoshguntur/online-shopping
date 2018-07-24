78f25bb5715baab4f83397e39687b577c832d720
santoshkumar.guntur <santoshkumar.guntur@N9659.intellectdesign.com>
2018-07-24 20:04:35 +0530

    Part 02 - 06 - Creating Master Page

--- onlineshopping/src/main/java/net/gsk/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/gsk/onlineshopping/controller/PageController.java
@@ -10,9 +10,26 @@ public class PageController {
 	@RequestMapping(value={"/","/index","/home"})
 	public ModelAndView index(){
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to spring MVC");
+		mv.addObject("title", "Home Page");
+		mv.addObject("userClickHome", true);
+		return mv;
+	}@RequestMapping(value={"/about"})
+	public ModelAndView about(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title", "Abount Us");
+		mv.addObject("userClickAbout", true);
 		return mv;
 	}
+	
+	@RequestMapping(value={"/contact"})
+	public ModelAndView contact(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+	
+	
 	
  /*   @RequestMapping(value="/test")
 	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting){
