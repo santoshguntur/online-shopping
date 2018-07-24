83071765d731fcfb2578a641741494a92c92fb8a
santoshkumar.guntur <santoshkumar.guntur@N9659.intellectdesign.com>
2018-07-24 15:55:35 +0530

    Till Part 02 - 03 Configuring Spring Bean Configuration File

---
+++ onlineshopping/src/main/java/net/gsk/onlineshopping/controller/PageController.java
@@ -0,0 +1,16 @@
+package net.gsk.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value={"/","/index","/home"})
+	public ModelAndView index(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to spring MVC");
+		return mv;
+	}
+}
